8670ff0019ee451635451f9f928fe35c72b7fa12
wamah <pythondeveloper21@gmail.com>
2023-03-06 23:55:40 +0000

    Fetchall function

--- movies_sqlite.py
+++ movies_sqlite.py
@@ -6,13 +6,22 @@ cursor = connection.cursor()
 
 # cursor.execute("INSERT INTO Movies VALUES ('Taxi Driver', 'Martin Scorsese', 1979)")
 
-cursor.execute("select * from Movies")
+#cursor.execute("select * from Movies")
 
-famousFilms = [('pulp Fiction', 'Quentin Tarantino', 1994),
-               ('Back to the future', 'Steven Spielberg', 1985),
-               ('Moonrise kingdom', 'Wes Anderson', 2012)]
+#famousFilms = [('pulp Fiction', 'Quentin Tarantino', 1994),
+ #              ('Back to the future', 'Steven Spielberg', 1985),
+  #             ('Moonrise kingdom', 'Wes Anderson', 2012)]
 
-print(cursor.fetchone())
+#print(cursor.fetchall())
+
+#cursor.executemany('INSERT INTO Movies VALUES (?, ?, ?)', famousFilms)
+#cursor.execute("select * from Movies")
+
+release_year =(1985 ,)
+
+cursor.execute("SELECT * FROM Movies WHERE year=?", release_year)
+
+print(cursor.fetchall())
 
 connection.commit()
 connection.close()
