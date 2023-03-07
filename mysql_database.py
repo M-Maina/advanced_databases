667505794928faceef33f8a78a6c4171c45bcf8a
wamah <pythondeveloper21@gmail.com>
2023-03-07 12:01:12 +0000

    adding new data

--- mysql_database.py
+++ mysql_database.py
@@ -10,12 +10,36 @@ def connect(db_name):
         database=db_name)
     except Error as e:
        print(e)
+       
+       
+def add_new_project(cursor, project_title, project_description, task_description):
+    project_data = (project_title, project_description)
+    cursor.execute('''INSERT INTO projects(title, description)
+                   VALUES(%S, %S)''', project_data)
+    
+    project_id = cursor.lastrowid
+    tasks_data = []
+    
+    for description in tasks_descriptions:
+        task_data = (project_id, description)
+        tasks_data.append(task_data)
+        
+    cursor.executemany('''INSERT INTO tasks(project_id, description)
+                       VALUES (%S %S)''', tasks_data)
 
 if __name__=='__main__':
    db = connect("projects")
    cursor = db.cursor()
    
+   tasks = ["Clean bathroom", "Clean kitchen", "Clean living room"]
+   add_new_project(cursor, "clean house", "Clean house by room", tasks)
+   db.commit()
+   
    cursor.execute("SELECT * FROM projects")
    project_records = cursor.fetchall()
    print(project_records)
+   
+   cursor.execute("SELECT * FROM tasks")
+   tasks_records = cursor.fetchall()
+   print(tasks_records)
    db.close()
